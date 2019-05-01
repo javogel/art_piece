6f4d5789760c441415ce9ca150bd1bea1111a4fe
Johannus Vogel <johannus.vogel@gmail.com>
2019-05-01 08:17:33 -0700

    working progress

--- awillabee.js
+++ awillabee.js
@@ -1,23 +1,28 @@
 const canvasSketch = require('canvas-sketch');
 const { lerp } = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
+const palettes = require('nice-color-palettes')
 
 const settings = {
   dimensions: [ 2048, 2048 ]
 };
 
 const sketch = () => {
+  const palette = random.pick(palettes)
 
   const createGrid = () =>{
     const points = [];
-    const count = 10;
+    const count = 100;
+    debugger;
     for (let x = 0; x < count; x++){
       for (let y = 0; y < count; y++){
         const u = x/(count-1);
         const v = y/(count-1);
-
+        const radius = Math.abs(random.noise2D(u,v)) * 0.025;
         points.push({
-          radius: Math.abs(random.gaussian()*0.2),
+          color: random.pick(palette),
+          radius,
+          rotation: random.noise2D(u,v),
           position:  [u,v]
         })
       }
@@ -37,21 +42,27 @@ const sketch = () => {
 
     points.forEach((data)=>{
 
-      const {position, radius } = data;
+      const {position, radius, color, rotation } = data;
       const [u,v] = position;
       const x=lerp(margin, width - margin, u);
       const y=lerp(margin, height - margin, v)
 
-      context.beginPath();
-      context.arc(x,y, radius * width,0,Math.PI*2, false);
-      const colorList = ["rgb(255,66, 14, 0.2)", "rgb(137,218, 89, 0.2)", "rgb(55,94, 151, 0.2)"]
-      const colorIndex = Math.floor(random.value() * colorList.length)
-      const color  =  colorList[colorIndex]
-      context.strokeStyle = "rgb(1, 1, 1, 0)";
+      // context.beginPath();
+      // context.arc(x,y, radius * width,0,Math.PI*2, false);
+
+      // context.strokeStyle = color;
+      // context.fillStyle = color;
+      // context.lineWidth = 4;
+      // context.stroke();
+      // context.fill();
+
+      context.save()
       context.fillStyle = color;
-      context.lineWidth = 4;
-      context.stroke();
-      context.fill();
+      context.font = `${radius * width}px Helvetica`
+      context.translate(x,y);
+      context.rotate(rotation);
+      context.fillText('+', 0, 0)
+      context.restore()
 
     })
   };
